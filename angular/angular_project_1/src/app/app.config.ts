a945e1c85cadf8891c5bb515d4838728164e1cd5
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-02-12 23:23:34 +0530

    angular day 1

--- angular/angular_project_1/src/app/app.config.ts
+++ angular/angular_project_1/src/app/app.config.ts
@@ -3,9 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// Project Level Configurations
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
 };
+
+
