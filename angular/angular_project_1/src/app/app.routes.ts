77b1031a56ab594cce782811559ca56caa904799
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-03-13 22:28:31 +0530

    march 13

--- angular/angular_project_1/src/app/app.routes.ts
+++ angular/angular_project_1/src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { Careers } from './components/careers/careers';
 import { Contactus } from './components/contactus/contactus';
 import { Notfound } from './components/notfound/notfound';
 import { UserList } from './components/user-list/user-list';
-import { ProductList } from './components/product-list/product-list';
+// import { ProductList } from './components/product-list/product-list';
 import { Userdetails } from './components/userdetails/userdetails';
 import { ProductDetails } from './components/product-details/product-details';
 import { PermanentJobs } from './components/permanent-jobs/permanent-jobs';
 import { ContractJobs } from './components/contract-jobs/contract-jobs';
 import { UploadVideos } from './components/upload-videos/upload-videos';
 import { trainerGuard } from './guards/trainer-guard';
+import { canExitGuard } from './guards/can-exit-guard';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
@@ -22,17 +23,24 @@ export const routes: Routes = [
     children: [
       { path: 'permanent', component: PermanentJobs },
       { path: 'contract', component: ContractJobs },
-    ]
+    ],
   },
-  { path: 'contactus', component: Contactus },
-  { path: 'products', component: ProductList },
+  {
+    path: 'contactus',
+    component: Contactus,
+    canDeactivate: [canExitGuard],
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./components/product-list/product-list').then((x) => x.ProductList)
+  },
   { path: 'productdetails', component: ProductDetails },
   { path: 'users', component: UserList },
   { path: 'userdetails/:id', component: Userdetails },
   {
     path: 'uploadvideos',
     component: UploadVideos,
-    canActivate: [trainerGuard]
+    canActivate: [trainerGuard],
   },
   { path: '', component: Home },
   { path: '**', component: Notfound },
--- angular/angular_project_1/src/app/components/contactus/contactus.ts
+++ angular/angular_project_1/src/app/components/contactus/contactus.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contactus.html',
   styleUrl: './contactus.css',
 })
 export class Contactus {
+  user = {
+    firstName: 'Virat',
+    lastName: 'Kohli',
+    email: 'sanjay@gmail.com',
+  };
 
+  hasChanges: boolean = true;
+
+  submitMyForm(data: any) {
+    console.log(data);
+    this.hasChanges = false;
+  }
 }
