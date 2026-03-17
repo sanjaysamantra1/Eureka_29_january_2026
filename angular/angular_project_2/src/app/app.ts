48da1f1cf4eb55dc0737eb83c8b49aa38c42122e
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-03-17 22:01:59 +0530

    employee crud

--- angular/angular_project_2/src/app/app.ts
+++ angular/angular_project_2/src/app/app.ts
@@ -2,13 +2,17 @@ import { Component, signal } from '@angular/core';
 import { MaterialDemo } from './components/material-demo/material-demo';
 import { Counter } from "./components/counter/counter";
 import { TodoCrud } from './components/todo-crud/todo-crud';
+import { EmployeeList } from './components/employee-list/employee-list';
+import { EmployeeAdd } from './components/employee-add/employee-add';
 
 @Component({
   selector: 'app-root',
   imports: [
     // MaterialDemo,
     // Counter
-    TodoCrud
+    // TodoCrud
+    EmployeeList,
+    EmployeeAdd
 ],
   templateUrl: './app.html',
   styleUrl: './app.css',
